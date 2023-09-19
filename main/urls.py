d1e1c8099094a48a6a4d8fb9b725a968b8c0e95a
HezBoomin <hezekial7778@gmail.com>
2023-09-19 22:43:55 +0700

    Updated urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,9 +7,9 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create_product/', create_product, name='create_product'),
-    path('show_xml/', show_xml, name='show_xml'),
-    path('show_json/', show_json, name='show_json'),
-    path('show_xml_by_id/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('show_json_by_id/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('delete/<int:id>/', delete_item, name='delete_item'),
 ]
